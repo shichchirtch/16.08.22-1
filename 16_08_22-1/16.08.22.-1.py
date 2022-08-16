f80332ffa0226889625bfa98bc8d05130a2df8b5
shichchirtch <ivanvol9l@gmail.com>
2022-08-16 19:09:06 +0400

    second commit

--- 16_08_22-1/16.08.22.-1.py
+++ 16_08_22-1/16.08.22.-1.py
@@ -2,13 +2,32 @@ class Track:
     def __init__(self, x=0, y=0):
         self.start_x = x
         self.start_y = y
+        self.arr = []
 
     def add_track(self, tr):
-        pass
-
+        if type(tr) == TrackLine:
+            self.arr.append(tr)
 
     def get_tracks(self):
-        pass
+        return tuple(self.arr)
+
+    def get_lenth(self, i):
+        return (((self.arr[i - 1].to_x - self.arr[i].to_x) ** 2 + (
+                    self.arr[i - 1].to_y - self.arr[i].to_y) ** 2) ** 0.5)
+
+    def __len__(self):
+        len_1 = ((self.start_x - self.arr[0].to_x) ** 2 + (self.start_y - self.arr[0].to_y) ** 2) ** 0.5
+        s = 0
+        for i in range(1, len(self.arr)):
+            x = self.get_lenth(i)
+            s += x
+        return int(len_1 + s)
+
+    def __eq__(self, other):
+        return len(self) == len(other)
+
+    def __lt__(self, other):
+        return len(self) < len(other)
 
 
 class TrackLine:
@@ -20,12 +39,17 @@ class TrackLine:
             self.max_speed = max_speed
 
 
-
-#((x1 - x0)**2 + (y1 - y0)**2) ** 0.5
+# ((x1 - x0)**2 + (y1 - y0)**2) ** 0.5
 
 track1, track2 = Track(), Track(0, 1)
+
 track1.add_track(TrackLine(2, 4, 100))
 track1.add_track(TrackLine(5, -4, 100))
+
 track2.add_track(TrackLine(3, 2, 90))
 track2.add_track(TrackLine(10, 8, 90))
+
 res_eq = track1 == track2
+print(res_eq)
+print((len(track1)))
+print(len(track2))
